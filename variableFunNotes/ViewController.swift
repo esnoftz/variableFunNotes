73ef4ba56242314b8369f3482effb516b3a30af0
Evangeline Noftz <esnoftz@gmail.com>
2024-08-26 08:13:17 -0500

    Initial Commit

---
+++ variableFunNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  variableFunNotes
+//
+//  Created by EVANGELINE NOFTZ on 8/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
